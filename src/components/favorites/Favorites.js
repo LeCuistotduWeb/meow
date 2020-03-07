7f872a7d0f4148ffb17b05fa97b8fe1917672de0
lecuistotduweb <gaetan.boyron@gmail.com>
2020-03-07 16:52:11 +0100

    add favorite page

--- src/components/favorites/Favorites.js
+++ src/components/favorites/Favorites.js
@@ -1,10 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Favorites extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            favorites: []
+        }
+    }
+
+    componentDidMount() {
+        localStorage.getItem('isFavorite') ? this.setState({favorites: JSON.parse(localStorage.getItem('isFavorite'))}) : this.setState({favorites:[]})
+    }
+
     render() {
+        
+        const {favorites} = this.state
+
         return (
-            <div>
-                mes favoris
+            <div className={`favorites__container ${favorites.length > 1 ? 'grid': ''}`}>
+                {favorites.length > 1 ? (
+                    favorites.map(elt => 
+                        <div className={"favorite-item"}>
+                            <img src={elt.url} alt={elt.id}/>
+                        </div>
+                    )
+                ) : 
+                (
+                    <div className="favorites__content-null">
+                        <p>Vous avez aucun favoris pour le momment</p>
+                        <Link to="/chatroulette">Jouer à Chatroulette</Link>
+                    </div>
+                )}
             </div>
         )
     }
